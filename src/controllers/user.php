fb5067e19c8e50ed8dfff4be39fa895cd64f3218
qzxst <1120398127@qq.com>
2023-12-14 01:10:34 +0800

    更新

--- src/controllers/user.php
+++ src/controllers/user.php
@@ -3,14 +3,23 @@
 namespace Blr\Php\Controllers;
 
 use Blr\Php\Models\User as UserModel;
+use MillionMile\GetEnv\Env;
 
 class User
 {
   public function index()
   {
-    $user = new UserModel();
-
-    $user->test();
+    $conf = [
+      'type' => Env::get('DB_TYPE', 'mysql'),
+      'database' => Env::get('DB_NAME','test'),
+      'host' => Env::get('DB_HOST', 'mysql'),
+      'username' => Env::get('DB_USER', 'root'),
+      'password' => Env::get('DB_PASS', '123456'),
+      'port' => Env::get('DB_PORT', '3306')
+    ];
+    $user = new UserModel($conf);
+    var_dump($user);
+    exit;
   }
   /**
    * Class constructor.
